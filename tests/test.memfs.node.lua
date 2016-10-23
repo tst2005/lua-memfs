17fda361a70ab940171c27a24a1767b04c184349
TsT <tst2005@gmail.com>
2016-10-23 03:24:52 +0200

    clean up

--- tests/test.memfs.node.lua
+++ tests/test.memfs.node.lua
@@ -1,58 +1,13 @@
-local node = require "memfs.node-orig"
-local r0 = node(true) -- the root directory (usualy asked "/")
+local node = require "memfs.node"
 
-local function printdir(k,v, pdir)
-	pdir=pdir or ""
-	print(v, pdir.."/"..k, (k~="." and k~=".." and v.hardcount or ""))
-end
-local function rprint(k,v, pdir)
-	printdir(k,v,pdir)
-	if type(v)=="table" and k~="." and k~=".." then
-		v:all(rprint, pdir.."/"..k)
-	end
-end
+local n0 = require"mini.class".instance(node)
 
 
-r0:mkdir"foo"		-- /foo
-r0:mkdir"bar"		-- /bar
-r0"bar":mkdir"rrr"	-- /bar/rrr
-
---[[
-assert(r0 "."  == r0)
-assert(r0 ".." == r0)
-
-assert(r0 "foo" "."  == r0 "foo")
-assert(r0 "foo" ".." == r0)
-
-assert(r0 "bar" "."  == r0 "bar")
-assert(r0 "bar" ".." == r0)
-
-assert(r0 "bar" "rrr" "."  == r0 "bar" "rrr")
-assert(r0 "bar" "rrr" ".." == r0 "bar")
-]]--
-
---
-
-assert(r0/"."  == r0 ".")
-do	local name = "."
-	assert(r0/name == r0(name))
-end
-assert(r0/"."  == r0)
-assert(r0/".." == r0)
-
-assert(r0/"foo"/"."  == r0 "foo")
-assert(r0/"foo"/".." == r0)
-
-assert(r0/"bar"/"."  == r0 "bar")
-assert(r0/"bar"/".." == r0)
-
-assert(r0/"bar"/"rrr"/"."  == r0 "bar" "rrr")
-assert(r0/"bar"/"rrr"/".." == r0 "bar")
-
-
-r0:all(rprint, "")
-print("rmdir rrr")
-r0("bar"):rmdir("rrr")
-r0:all(rprint, "")
+assert(n0.hardlink)
+assert(n0.isfile)
+assert(n0.isdir)
+assert(n0.hardlink)
+assert(n0.unhardlink)
+assert(n0.all)
 
 print("OK")
